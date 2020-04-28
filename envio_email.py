3b9707eb84b77d8745cc53042b2bdba933bc7915
vitateje <vitateje@gmail.com>
2020-04-27 23:36:22 -0300

    envio de email

---
+++ envio_email.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.mime.text import MIMEText
+
+# conexão com os servidores do google
+smtp_ssl_host = 'smtp.gmail.com'
+smtp_ssl_port = 465
+# username ou email para logar no servidor
+username = 'usuario'
+password = 'senha'
+
+from_addr = 'origin@gmail.com'
+to_addrs = ['destiny@gmail.com']
+
+# a biblioteca email possuí vários templates
+# para diferentes formatos de mensagem
+# neste caso usaremos MIMEText para enviar
+# somente texto
+message = MIMEText('Hello World')
+message['subject'] = 'Hello'
+message['from'] = from_addr
+message['to'] = ', '.join(to_addrs)
+
+# conectaremos de forma segura usando SSL
+server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+# para interagir com um servidor externo precisaremos
+# fazer login nele
+server.login(username, password)
+server.sendmail(from_addr, to_addrs, message.as_string())
+server.quit()
